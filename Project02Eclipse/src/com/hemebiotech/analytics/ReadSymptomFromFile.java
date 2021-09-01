58a9cdc768b96dcf7243a56620b2b8923adc9de3
ezarath <antoine.goy86@gmail.com>
2021-09-01 19:18:29 +0200

    aled

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomFromFile.java
@@ -10,38 +10,31 @@ import java.util.List;
  * Simple brute force implementation
  *
  */
-public class ReadSymptomDataFromFile implements ISymptomReader {
+public class ReadSymptomFromFile implements ISymptomReader {
 
-	private String filepath;
 	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
-	}
 	
 	@Override
 	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
 		
-		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+		ArrayList<String> namesOfSymptoms = new ArrayList<>();
+		
+		try {
+				BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse\\symptoms.txt"));
 				String line = reader.readLine();
 				
+				
 				while (line != null) {
-					result.add(line);
+					namesOfSymptoms.add(line);
 					line = reader.readLine();
 				}
 				reader.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}
 		
-		return result;
+		
+		return namesOfSymptoms;
 	}
 
 }
